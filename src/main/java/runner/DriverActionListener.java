39c55e7102bdd780fdac2d320228043bf64d7ab5
gyapeee <gyapeee@gmail.com>
2025-03-02 22:15:26 +0100

    Test 5 api test is implemented

--- src/main/java/runner/DriverActionListener.java
+++ src/main/java/runner/DriverActionListener.java
@@ -1,25 +1,26 @@
 package runner;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.support.events.WebDriverListener;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
+@Slf4j
 public class DriverActionListener implements WebDriverListener {
-    private static final Logger log = LoggerFactory.getLogger(DriverActionListener.class);
 
-    @Override
-    public void beforeAnyCall(Object target, Method method, Object[] args) {
-        log.info(String.format("Thread: %s | Method Name: %s | Method Args: %s",
-                Thread.currentThread().getName(), method.getName(), Arrays.toString(args)));
-    }
+  @Override
+  public void beforeAnyCall(Object target, Method method, Object[] args) {
+    log.info(String.format("Thread: %s | Method Name: %s | Method Args: %s", Thread.currentThread()
+                                                                                   .getName(), method.getName(),
+                           Arrays.toString(args)));
+  }
 
-    @Override
-    public void afterAnyCall(Object target, Method method, Object[] args, Object result) {
-        log.info(String.format("Thread: %s | Method Name: %s | Method Args: %s",
-                Thread.currentThread().getName(), method.getName(), Arrays.toString(args)));
-    }
+  @Override
+  public void afterAnyCall(Object target, Method method, Object[] args, Object result) {
+    log.info(String.format("Thread: %s | Method Name: %s | Method Args: %s", Thread.currentThread()
+                                                                                   .getName(), method.getName(),
+                           Arrays.toString(args)));
+  }
 }
